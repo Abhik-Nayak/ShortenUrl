8803e9f39e097c573502e4eb74286cc7ff1f7ef3
Abhik Nayak <abhiknayakabhik@gmail.com>
2026-07-14 16:03:13 +0530

    remove old data

--- url-shortener-sever/src/routes/urlRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createShortUrl, redirectToOriginalUrl } from "../controllers/urlController";
-
-const router = Router();
-
-router.post("/shorten", createShortUrl);
-router.get("/:shortCode", redirectToOriginalUrl);
-
-export default router;
